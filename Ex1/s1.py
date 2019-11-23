9283d7af9e1565bc98ac27665c8f40e88ba82bfb
duongvpyltk <49305668+duongvpyltk@users.noreply.github.com>
2019-11-23 20:31:03 +0700

    s1

---
+++ Ex1/s1.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+#Tần số lấy mẫu
+fs = 1000
+#Thời gian đo
+t = np.arange(-1, 1, 1/fs)
+#Biên độ tín hiệu
+A = 5
+#Tần số cơ bản
+f = 5
+#Tiến hiệu s1(t)
+s1 = A * np.sin(2 * math.pi * f * t)
+#Vẽ hình
+plt.plot(t, s1)
+plt.title('s1(t) = Asin(2πft)')
+plt.xlabel('t = 1/fs')
+plt.ylabel('s1(t)')
+plt.show()

92dc712d950526c5bacdf78d721fe4921bc0008e
duongvpyltk <49305668+duongvpyltk@users.noreply.github.com>
2019-11-23 22:46:00 +0700

    ex1

--- Ex1/s2.py
+++ Ex1/s2.py
@@ -10,11 +10,16 @@ t = np.arange(-1, 1, 1/fs)
 A = 100
 #Tần số cơ bản
 f = 5
-#Tiến hiệu s1(t)
-s1 = A * np.sin(2 * math.pi * f * t)
+#Số tìu ý
+n = 500
+#Tiến hiệu s2(t)
+def sin(x):
+    sum = 0
+    for i in range(0,2*n+1,1):
+        sum = sum + A/math.pow(2*i+1, 2) * np.sin(2*math.pi*(2*i+1)*f*x)
+    return sum
 #Vẽ hình
-plt.plot(t, s1)
-plt.title('s1(t) = Asin(2πft)')
+plt.plot(t, sin(t))
 plt.xlabel('t = 1/fs')
-plt.ylabel('s1(t)')
+plt.ylabel('s2(t)')
 plt.show()

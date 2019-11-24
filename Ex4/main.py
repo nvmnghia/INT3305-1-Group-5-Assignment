030ee5fae929a9910df276a347fbd9a81bba6efe
tdhoc <lhpwind@gmail.com>
2019-11-24 23:15:01 +0700

    Update Ex4

--- Ex4/main.py
+++ Ex4/main.py
@@ -1,30 +1,23 @@
 from PIL import Image
 import numpy as np
 import scipy.fftpack as fp
+from matplotlib import pylab
 
-## Functions to go from image to frequency-image and back
 im2freq = lambda data: fp.rfft(fp.rfft(data, axis=0),
                                axis=1)
 freq2im = lambda f: fp.irfft(fp.irfft(f, axis=1),
                                          axis=0)
 
-## Read in data file and transform
 data = np.array(Image.open('test.png'))
 
-freq = im2freq(data)
-back = freq2im(freq)
-assert (np.allclose(data, back))
-
-## Helper functions to rescale a frequency-image to [0, 255] and save
-remmax = lambda x: x / x.max()
-remmin = lambda x: x - np.amin(x, axis=(0, 1), keepdims=True)
-touint8 = lambda x: (remmax(remmin(x)) * (256 - 1e-4)).astype(int)
-
-
-def arr2im(data, fname):
-    out = Image.new('RGB', data.shape[1::-1])
-    out.putdata(map(tuple, data.reshape(-1, 3)))
-    out.save(fname)
-
-
-arr2im(touint8(freq), 'freq.png')
+pylab.figure(figsize=(12,10))
+freq2 = fp.fft2(data)
+data_ = fp.ifft2(freq2).real
+pylab.subplot(2,2,1), pylab.imshow(data, cmap='gray'), pylab.title('original image', size = 20)
+pylab.subplot(2,2,2), pylab.imshow(20*np.log10( 0.01 + np.abs(fp.fftshift(freq2))), cmap='gray')
+pylab.title('FFT Spectrum Magnitude', size = 20)
+pylab.subplot(2,2,3), pylab.imshow(np.angle(fp.fftshift(freq2)), cmap = 'gray')
+pylab.title('FFT Phase', size = 20)
+pylab.subplot(2,2,4), pylab.imshow(np.clip(data_, 0,255), cmap = 'gray')
+pylab.title('Reconstructed Image', size = 20)
+pylab.show()
